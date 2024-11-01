a1cb0fd123eb958ea257c5049080409290ec72a7
サカリ <veikka.junttila1@gmail.com>
2024-11-01 17:55:50 +0900

    configure redis getAsync setAsync calls

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -6,14 +6,39 @@ const configs = require('../util/config')
 
 /* GET index data. */
 router.get('/', async (req, res) => {
-  redis.setAsync('visits', 23)
 
-  visits = redis.getAsync('visits')
+  visits = await redis.getAsync('visits')
+
   console.log(visits)
+  
+  if (isNaN(visits)) {
+    await redis.setAsync('visits', 0)
+  }
+
+  visits = await redis.getAsync('visits')
+
+  redis.setAsync('visits', parseInt(visits)+1)
+
+  visits = await redis.getAsync('visits')
 
   res.send({
-    ...configs,
-    ...redis.getAsync('visits')
+    ...configs
+  });
+});
+
+/* GET statistics. */
+router.get('/statistics', async (req, res) => {
+
+  added_todos = await redis.getAsync('added_todos')
+  
+  if (isNaN(added_todos)) {
+    await redis.setAsync('added_todos', 0)
+  }
+
+  added_todos = await redis.getAsync('added_todos')
+
+  res.send({
+    added_todos
   });
 });
 
